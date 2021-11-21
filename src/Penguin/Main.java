c33c5691ecfa446421c6d859545b9df6726a1edc
XaGo37 <alexandre.santosgom@gmail.com>
2021-11-21 21:29:54 +0100

    Sans l'erreur de déplacement

--- src/Penguin/Main.java
+++ src/Penguin/Main.java
@@ -1,13 +1,14 @@
 package Penguin;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Personnage perso = new Personnage();
-        Map worldMap = new Map(15, 19);
+        Map worldMap = new Map(5, 5);
 
         while (perso.getXPerso() < worldMap.nblig && perso.getYPerso() < worldMap.nbcol) {
             worldMap.placer(perso.getYPerso(), perso.getXPerso(), 'J');
